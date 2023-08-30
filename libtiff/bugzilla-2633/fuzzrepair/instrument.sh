2dd3efd08b3e9749ccdfbd65c9136a98ee60087e
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-08-30 14:40:44 +0800

    add placeholder instrumentation to setup script

--- libtiff/bugzilla-2633/fuzzrepair/instrument.sh
+++ libtiff/bugzilla-2633/fuzzrepair/instrument.sh
@@ -7,15 +7,12 @@ dir_name=$1/$benchmark_name/$project_name/$bug_id
 
 fix_file=$dir_name/src/$2
 
-# (1) patch another (unrelated) bug
 cd $dir_name/src
-sed -i '2695s/^.*$/while (cc-- > 1) {/' tools/tiff2ps.c
 
 # (2) add instrumentation for fuzzrepair
 $script_dir/../config.sh $1
 cd $dir_name/src
-sed -i '2470i if(0) return;' tools/tiff2ps.c
-sed -i '2441i if(0) return;' tools/tiff2ps.c
+
 make clean
 
 bear $script_dir/../build.sh $1

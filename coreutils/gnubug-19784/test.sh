8e7fa252c9e5bd6efd8200495ed090160550341d
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2023-09-08 12:59:58 +0800

    set root path to /experiment

--- coreutils/gnubug-19784/test.sh
+++ coreutils/gnubug-19784/test.sh
@@ -3,17 +3,17 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
 project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
-dir_name=$1/$benchmark_name/$project_name/$bug_id
+dir_name=/experiment/$benchmark_name/$project_name/$bug_id
 BINARY_PATH="$dir_name/src/src/make-prime-list"
-TEST_ID=$2
+TEST_ID=$1
 
-if [ -n "$3" ];
+if [ -n "$2" ];
 then
-  BINARY_PATH=$3
+  BINARY_PATH=$2
 fi
 
 
-case "$2" in
+case "$TEST_ID" in
     1)
         POC="15"
         export ASAN_OPTIONS=detect_leaks=0,halt_on_error=0

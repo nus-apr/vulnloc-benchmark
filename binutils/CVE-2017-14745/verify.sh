a3616437c1c58dc813407ef3db2f89516ecaa272
Martin Mirchev <mirchevmartin2203@gmail.com>
2023-12-06 14:22:56 +0200

    Update verify.sh script

--- binutils/CVE-2017-14745/verify.sh
+++ binutils/CVE-2017-14745/verify.sh
@@ -4,12 +4,12 @@ benchmark_name=$(echo $script_dir | rev | cut -d "/" -f 3 | rev)
 project_name=$(echo $script_dir | rev | cut -d "/" -f 2 | rev)
 bug_id=$(echo $script_dir | rev | cut -d "/" -f 1 | rev)
 dir_name=/experiment/$benchmark_name/$project_name/$bug_id
-
+TEST_ID=$1
 
 cd $dir_name/src
 patch -f -p 1 < $dir_name/dev-patch/fix.patch
 bash $script_dir/build.sh
-bash $script_dir/test.sh 1
+bash $script_dir/test.sh $TEST_ID
 ret=$?
 patch -R -f -p 1 < $dir_name/dev-patch/fix.patch
 bash $script_dir/build.sh
